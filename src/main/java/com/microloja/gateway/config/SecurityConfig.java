fb4d4c981811faa826e8125811558f7ae9525e84
guidev1911 <guilhermereal1911@gmail.com>
2025-08-15 19:07:20 -0300

    correção keycloak KEYCLOAK_ISSUER_URI/ ReactiveJwtDecoder

--- src/main/java/com/microloja/gateway/config/SecurityConfig.java
+++ src/main/java/com/microloja/gateway/config/SecurityConfig.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.config.Customizer;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    private static final String KEYCLOAK_ISSUER_URI = "http://keycloak:8080/realms/microloja";
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
@@ -21,9 +24,16 @@ public class SecurityConfig {
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())
+                        .jwt(jwt -> jwt
+                                .jwtDecoder(jwtDecoder())
+                        )
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        return NimbusReactiveJwtDecoder.withJwkSetUri(KEYCLOAK_ISSUER_URI + "/protocol/openid-connect/certs").build();
     }
 }
